29a9699419dba52a3c56d1b849cd3690722aa579
Haplo064 <haplo064@gmail.com>
2016-06-14 17:05:10 +0930

    Add files via upload

--- tempfix.rb
+++ tempfix.rb
@@ -231,7 +231,6 @@ bot.command(:userlist, min_args: 0, max_args: 1, description: "Shows the user da
     if pages<1 then pages=1 end
     if page>=pages then page=0 end
     i=(users.length/pages)*page
-    puts i
     event << "User Database:"
     event << "`Name               IGN                 Guild               Timezone`"
       begin
@@ -250,6 +249,39 @@ bot.command(:userlist, min_args: 0, max_args: 1, description: "Shows the user da
       event << "Showing page #{page+1}/#{pages}"
     end
     puts "#{clock.inspect}: #{event.user.name}: -userlist <#{page}>"
+end
+
+#Find users in the database.
+bot.command(:userfind, min_args: 1, max_args: 1) do |event, search|
+  i=0
+  found=[]
+  begin
+    if users[i].id!=nil && users[i].id==search then found.push(users[i]) end
+    if users[i].name!=nil && users[i].name.downcase.include?(search.downcase) then found.push(users[i]) end
+    if users[i].ign!=nil && users[i].ign.downcase.include?(search.downcase) then found.push(users[i]) end
+    if users[i].guild!=nil && users[i].guild.downcase.include?(search.downcase) then found.push(users[i]) end
+    if users[i].timezone!=nil && users[i].timezone.downcase.include?(search.downcase) then found.push(users[i]) end
+    i+=1
+  end while users[i]!=nil
+  found.uniq!
+  if !found.empty?
+    i=0
+    event << "Found User Database:"
+    event << "`Name               IGN                 Guild               Timezone`"
+      begin
+        str = ""
+        if found[i].name!=nil then str << "`#{found[i].name.to_s}" end
+        str=str.ljust(20)
+        if found[i].ign!=nil then str << "#{found[i].ign.to_s}" end
+        str=str.ljust(40)
+        if found[i].guild!=nil then str << "#{found[i].guild.to_s}" end
+        str=str.ljust(60)
+        if found[i].timezone!=nil then str << "#{found[i].timezone.to_s}" end
+        if found[i].name!=nil then str << "`" end
+        event << str
+        i+=1
+      end while i < found.length
+  end
 end
 #Time to reset
 bot.command(:time) do |event|
